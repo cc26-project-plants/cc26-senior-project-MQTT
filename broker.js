c872fdec81134f1a212c8159506e080d27b468d4
Sakiko Yoshida <sacco221@gmail.com>
2022-06-09 11:27:05 +0900

    feat: change http server to https server

--- broker.js
+++ broker.js
@@ -1,30 +1,35 @@
 import aedes from "aedes";
 const aedesSrv = aedes();
 import { createServer } from "aedes-server-factory";
+import websocketStream from "websocket-stream";
+import https from "https";
+import tls from "tls";
 
 // 'aedes-server-factory' tcp mqtt
-const port1 = 1883;
-const server = createServer(aedesSrv);
+// const port1 = 1883;
+// const server = createServer(aedesSrv);
 
 //'aedes-server-factory' ws
 // const port = 8888;
 // const httpServer = createServer(aedesSrv, { ws: true });
-
-// WSS
-import https from "https";
 
 const options = {
   key: process.env.PRODUCTION_KEY,
   cert: process.env.PRODUCTION_CERT,
 };
 
+// WSS
 const httpsServer = https.createServer(options, (req, res) => {
   res.writeHead(200);
   res.end("hello Happa\n");
 });
-import websocketStream from "websocket-stream";
+
 const port2 = 8808;
 websocketStream.createServer({ server: httpsServer }, aedesSrv.handle);
+
+//TLS
+const port3 = 8883;
+const tlsServ = tls.createServer(options, aedesSrv.handle);
 
 aedesSrv.on("clientError", (client, error) => {
   console.error(`MQTT client error `, client.id);
@@ -91,14 +96,20 @@ aedesSrv.on("clientDisconnect", (client) => {
 //   );
 // });
 
-server.listen(port1, () => {
-  console.log(
-    `MQTT Broker Aedes TCP started and is listening on port ${port1} ....`
-  );
-});
+// server.listen(port1, () => {
+//   console.log(
+//     `MQTT Broker Aedes TCP started and is listening on port ${port1} ....`
+//   );
+// });
 
 httpsServer.listen(port2, () => {
   console.log(
     `MQTT Broker Aedes over websocket secured started and is listening on port ${port2} ....`
+  );
+});
+
+tlsServ.listen(port3, () => {
+  console.log(
+    `MQTT Broker Aedes over websocket started and is listening on port ${port3} ....`
   );
 });
